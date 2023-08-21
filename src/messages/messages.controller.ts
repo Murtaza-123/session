0c5a19445f9bc120835141e131e40c51b30ea288
Murtaza-123 <81867986+Murtaza-123@users.noreply.github.com>
2023-08-21 13:24:04 +0500

    New feature added (#1)

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,28 +1,16 @@
-import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { MessagesService } from './messages.service';
-import { Request } from 'express';
-import { MessageDto } from 'src/Dto/create-message-dto';
-import { ApiProperty } from '@nestjs/swagger';
 
 @Controller('api/v1/talking')
 export class MessagesController {
-  //constructor(private messageService: MessagesService) {}
- // @ApiProperty()
- // @Post('client')
-  // async sendMessageToBot(
-  //   @Req() req: Request,
-  //   @Body() createMessage: MessageDto,
-  // ) {
-  //   return this.messageService.handleClientMessage(req, createMessage);
-  // }
+  constructor(private messageService: MessagesService)
+  {
 
-
-  // @ApiProperty()
-  // @Post('bot')
-  // async sendMessageToClient(
-  //   @Req() req: Request,
-  //   @Body() createMessage: MessageDto,
-  // ) {
-  //   return this.messageService.sendBotResponseToClient(req, createMessage);
-  // }
+  }
+  @Get()
+  async findId(id:number): Promise<boolean>
+  {
+    const result = await this.messageService.findId(id);
+    return result;
+  }
 }
